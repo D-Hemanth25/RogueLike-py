0c97f148de4ca564a8c6020143899fa38d2110c6
D-Hemanth25 <mailhemanth25@gmail.com>
2024-10-31 17:19:52 +0530

    Implement Movements

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import tcod
 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
 def main() -> None:
     screenWidth = 80
     screenHeight = 50
 
+    # we could also use // for integer division but wrapping values in int() will always guarantee an int
+    playerX, playerY = int(screenWidth / 2), int(screenHeight / 2)
+
     tileset = tcod.tileset.load_tilesheet(
         "assets/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+
+    eventHandler = EventHandler()
 
     with tcod.context.new_terminal(
         screenWidth,
@@ -18,11 +26,19 @@ def main() -> None:
         # default order is [y, x] wiht order="F" the order becomes [x, y]
         rootConsole = tcod.Console(screenWidth, screenHeight, order="F") 
         while True:
-            rootConsole.print(x=1, y=1, string="@")
+            rootConsole.print(x=playerX, y=playerY, string="@")
             context.present(rootConsole)
 
             for event in tcod.event.wait():
-                if event.type == "QUIT":
+                action = eventHandler.dispatch(event)
+
+                if not action: continue
+
+                if isinstance(action, MovementAction):
+                    playerX += action.dx
+                    playerY += action.dy
+                
+                elif isinstance(action, EscapeAction):
                     raise SystemExit()
 
 if __name__ == "__main__":
